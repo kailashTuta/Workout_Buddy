7e5c875cc513dc89fc38c81b290c038840abe800
kailashtuta <kailashtuta2000@gmail.com>
2022-06-27 09:17:13 +0530

    Fetch Data From backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-// Pages and Components
-import Home from './pages/Home';
+// Pages & Components
+import Home from './pages/Home'
 import Navbar from './components/Navbar'
 
-
-
 function App() {
+
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />} />
-
+            <Route
+              path="/"
+              element={<Home />}
+            />
           </Routes>
         </div>
       </BrowserRouter>
