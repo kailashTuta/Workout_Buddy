c8900bdfa7d8b540b16a25d1b66434ab9b067161
kailashtuta <kailashtuta2000@gmail.com>
2022-07-01 15:45:16 +0530

    Added Add Workout With Global Context

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,62 +1,75 @@
-const Workout = require("../models/WorkoutModel");
-const mongoose = require("mongoose");
+const Workout = require('../models/WorkoutModel')
+const mongoose = require('mongoose')
 
-// GET All Workout
+// Get All Workouts
 const getWorkouts = async (req, res) => {
-    const workouts = await Workout.find({}).sort({ createdAt: -1 });
-    res.status(200).json(workouts);
-};
+    const workouts = await Workout.find({}).sort({ createdAt: -1 })
+
+    res.status(200).json(workouts)
+}
 
 // Get A Single Workout
 const getWorkout = async (req, res) => {
-    const { id } = req.params;
+    const { id } = req.params
     if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({ error: "No Such Workout" });
+        return res.status(404).json({ error: 'No such workout' })
     }
-    const workout = await Workout.findById(id);
-    if (!workout) {
-        return res.status(404).json({ error: "No Such Workout " });
-    }
-    res.status(200).json(workout);
-};
+    const workout = await Workout.findById(id)
 
-// Create New Workout
-const createWorkout = async (req, res) => {
-    const { title, load, reps } = req.body;
-    try {
-        const workout = await Workout.create({ title, load, reps });
-        res.status(200).json(workout);
-    } catch (error) {
-        res.status(400).json(error.message);
+    if (!workout) {
+        return res.status(404).json({ error: 'No such workout' })
     }
-};
+    res.status(200).json(workout)
+}
+
+// Create A New Workout
+const createWorkout = async (req, res) => {
+    const { title, load, reps } = req.body
+
+    // add to the database
+    try {
+        const workout = await Workout.create({ title, load, reps })
+        res.status(200).json(workout)
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+}
 
 // Delete A Workout
 const deleteWorkout = async (req, res) => {
-    const { id } = req.params;
+    const { id } = req.params
+
     if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({ error: "No Such Workout" });
+        return res.status(400).json({ error: 'No such workout' })
     }
-    const workout = await Workout.findOneAndDelete({ _id: id });
+
+    const workout = await Workout.findOneAndDelete({ _id: id })
 
     if (!workout) {
-        return res.status(404).json({ error: "No Such Workout " });
+        return res.status(400).json({ error: 'No such workout' })
     }
-    res.status(200).json(workout);
-};
+
+    res.status(200).json(workout)
+}
 
 // Update A Workout
 const updateWorkout = async (req, res) => {
-    const { id } = req.params;
+    const { id } = req.params
+
     if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({ error: "No Such Workout" });
+        return res.status(400).json({ error: 'No such workout' })
     }
-    const workout = await Workout.findOneAndUpdate({ _id: id }, { ...req.body });
+
+    const workout = await Workout.findOneAndUpdate({ _id: id }, {
+        ...req.body
+    })
+
     if (!workout) {
-        return res.status(404).json({ error: "No Such Workout " });
+        return res.status(400).json({ error: 'No such workout' })
     }
-    res.status(200).json(workout);
-};
+
+    res.status(200).json(workout)
+}
 
 module.exports = {
     getWorkouts,
@@ -64,4 +77,4 @@ module.exports = {
     createWorkout,
     deleteWorkout,
     updateWorkout
-};
+}
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,12 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
 
 // Components
 import WorkoutDetails from "../components/WorkoutDetails"
+import WorkoutForm from "../components/WorkoutForm"
 
 const Home = () => {
-    const [workouts, setWorkouts] = useState(null)
+    const { workouts, dispatch } = useWorkoutsContext()
 
     useEffect(() => {
         const fetchWorkouts = async () => {
@@ -12,11 +14,12 @@ const Home = () => {
             const json = await response.json()
 
             if (response.ok) {
-                setWorkouts(json)
+                dispatch({ type: 'SET_WORKOUTS', payload: json })
             }
         }
+
         fetchWorkouts()
-    }, [])
+    }, [dispatch])
 
     return (
         <div className="home">
@@ -25,6 +28,7 @@ const Home = () => {
                     <WorkoutDetails workout={workout} key={workout._id} />
                 ))}
             </div>
+            <WorkoutForm />
         </div>
     )
 }
